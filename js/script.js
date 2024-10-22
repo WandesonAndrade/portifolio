5c2eac4bd81dadea05f007962d1c7329ad15c73d
WandesonAndrade <wandesonandrade33@gmail.com>
2024-10-22 18:02:40 -0300

    atualizacao do footer e inicio da atualização do meu

--- js/script.js
+++ js/script.js
@@ -1,35 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Navegação suave
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
 
-            const target = document.querySelector(this.getAttribute('href'));
-            const headerOffset = document.getElementById('menu-fixo').offsetHeight; // Altura do menu fixo
-            const elementPosition = target.getBoundingClientRect().top; // Posição do elemento alvo
-            const offsetPosition = elementPosition + window.pageYOffset - headerOffset; // Posição ajustada
+// Abre/fecha o menu ao clicar no ícone
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
 
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: "smooth"
-            });
-        });
+// Fecha o menu ao clicar em um item do menu
+document.querySelectorAll('.menu li a').forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('open'); // Fecha o menu ao clicar em um item
     });
-
-    // Transparência do menu ao rolar
-    const menuFixo = document.getElementById('menu-fixo');
-    const originalBackgroundColor = 'rgba(68, 68, 68, 1)'; // Cor original
-    const transparentBackgroundColor = 'rgba(68, 68, 68, 0.7)'; // Cor transparente
-
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 0) {
-            menuFixo.classList.add('menu-transparente');
-            menuFixo.style.backgroundColor = transparentBackgroundColor; // Torna o fundo transparente
-        } else {
-            menuFixo.classList.remove('menu-transparente');
-            menuFixo.style.backgroundColor = originalBackgroundColor; // Retorna à cor original
-        }
-    });
-
-   
 });
