56e4cfc5e1fe075e351d37c819517a302ad4db21
WandesonAndrade <wandesonandrade33@gmail.com>
2024-10-23 09:40:15 -0300

    att menu

--- js/script.js
+++ js/script.js
@@ -11,4 +11,17 @@ document.querySelectorAll('.menu li a').forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('open'); // Fecha o menu ao clicar em um item
     });
+
+    // Seleciona o h2 do menu
+const menuH2 = document.querySelector('.menu-container h2');
+
+// Função que verifica a rolagem da página
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { // Se a rolagem for maior que 50px
+        menuH2.classList.add('small'); // Adiciona a classe 'small'
+    } else {
+        menuH2.classList.remove('small'); // Remove a classe 'small'
+    }
+});
+
 });
